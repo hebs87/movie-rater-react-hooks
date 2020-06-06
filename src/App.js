d147274403ff983c2300702ab86e441fd37d70d0
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-06 14:14:31 +0100

    Created movies list in useState hook and dynamically displayed data in movies list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [movies, setMovies] = useState(['Movie 1', 'Movie 2', 'Movie 3'])
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
       </header>
       <div className="layout">
-        <div>Movie List</div>
-        <div>Movie Details</div>
+        <div>
+          <h2>Movie List</h2>
+          {movies.map(movie => (
+            <p>{movie}</p>
+          ))}
+        </div>
+        <div>
+          <h2>Movie Details</h2>
+        </div>
       </div>
     </div>
   );
