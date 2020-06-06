fba3c98ef1f4d724f0562df3e6ede87a36197910
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-06 14:34:38 +0100

    Used useEffect hook to fetch API data, set the fetched data as state value and display in component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
 
-  const [movies, setMovies] = useState(['Movie 1', 'Movie 2', 'Movie 3'])
+  const [movies, setMovies] = useState([])
+
+  // Fetch data using useEffect hook and specify dependency for triggering it - empty array ensure it acts like
+  // componentDidMount() lifecycle method
+  useEffect(()=> {
+    fetch(`${process.env.REACT_APP_URL}api/movies/`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${process.env.REACT_APP_TOKEN}`
+        }
+      }
+    )
+      .then(res => res.json())
+      // Call the setMovies hook and set the value to the API movies data
+      .then(res => setMovies(res))
+      .catch(error => console.log(error))
+  }, [])
 
   return (
     <div className="App">
@@ -14,7 +32,7 @@ function App() {
         <div>
           <h2>Movie List</h2>
           {movies.map(movie => (
-            <p>{movie}</p>
+            <p key={movie.id}>{movie.title}</p>
           ))}
         </div>
         <div>
