06bb32f137b50d353d9bdbce16b27efa8b9ff0e2
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-07 00:18:13 +0100

    Added form fields to movie form and functionality to retrieve form field value and set as state

--- src/components/movie-form.jsx
+++ src/components/movie-form.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 const MovieForm = (props) => {
 
   const {movie} = props;
+  const [title, setTitle] = useState(movie.title);
+  const [description, setDescription] = useState(movie.description);
+
+  const updateClicked = () => {
+    console.log(title, description);
+  }
 
   return (
     <div>
       {
         movie &&
-        <h2>{movie.title}</h2>
+        <React.Fragment>
+          <label htmlFor="title">Title</label>
+          <br/>
+          <input id="title" type="text" placeholder="Title" value={title}
+                 onChange={event => setTitle(event.target.value)}/>
+          <br/>
+          <label htmlFor="description">Description</label>
+          <br/>
+          <textarea id="description" type="text" placeholder="Description" defaultValue={description}
+                    onChange={event => setDescription(event.target.value)}></textarea>
+          <br/>
+          <button onClick={updateClicked}>Update</button>
+        </React.Fragment>
       }
     </div>
   )
