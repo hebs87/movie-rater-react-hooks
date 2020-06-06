cd8837520e5e8ddf5893e24725092ee31d5b8319
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-06 18:04:56 +0100

    Used FontAwesome stars to display selected movie average rating in MovieDetails component

--- src/components/movie-details.jsx
+++ src/components/movie-details.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faStar} from "@fortawesome/free-solid-svg-icons";
 
 const MovieDetails = (props) => {
+
+  const {movie} = props;
 
   return (
     <div>
       {
         props.movie &&
         <React.Fragment>
-          <h2>{props.movie.title}</h2>
-          <p>{props.movie.description}</p>
+          <h2>{movie.title}</h2>
+          <p>{movie.description}</p>
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 0 ? 'orange': ''}/>
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 1 ? 'orange' : ''}/>
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 2 ? 'orange': ''}/>
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 3 ? 'orange': ''}/>
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 4 ? 'orange': ''}/>
+          <span>({movie.no_of_ratings})</span>
         </React.Fragment>
       }
-      {!props.movie && null}
+      {!movie && null}
     </div>
   )
 }
